419036548f37b98bd252f98efb89c0b65547c1d7
knowsarzehmeh <osarzeh@gmail.com>
2019-02-23 18:04:03 +0100

    new mongodb url Update

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
-
-const dbhost =
-  'mongodb://nosa:nosa%40123@cluster0-shard-00-00-av5ku.mongodb.net:27017,cluster0-shard-00-01-av5ku.mongodb.net:27017,cluster0-shard-00-02-av5ku.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
+const userName = 'nosa';
+const password = encodeURIComponent('nosa@123');
+const dbhost = `mongodb://${userName}:${password}@cluster0-shard-00-00-av5ku.mongodb.net:27017,cluster0-shard-00-01-av5ku.mongodb.net:27017,cluster0-shard-00-02-av5ku.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true`;
 // 'mongodb+srv://nosa:nosa%40123@cluster0-av5ku.mongodb.net/test?retryWrites=true';
 // process.env.TODOAPP_MONGODB_URI
 mongoose.Promise = global.Promise;
