0862a858a00ba1ba6d8b4a3fed1a782652b71348
knowsarzehmeh <osarzeh@gmail.com>
2019-02-23 18:19:14 +0100

    set env for mongodb

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,14 +1,18 @@
 const mongoose = require('mongoose');
 const userName = 'nosa';
 const password = encodeURIComponent('nosa@123');
-const dbhost = `mongodb://${userName}:${password}@cluster0-shard-00-00-av5ku.mongodb.net:27017,cluster0-shard-00-01-av5ku.mongodb.net:27017,cluster0-shard-00-02-av5ku.mongodb.net:27017/TodoApp?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true`;
+console.log(password);
+const dbhost = `mongodb://nosa:nosa%40123@cluster0-shard-00-00-av5ku.mongodb.net:27017,cluster0-shard-00-01-av5ku.mongodb.net:27017,cluster0-shard-00-02-av5ku.mongodb.net:27017/TodoApp?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true`;
 // 'mongodb+srv://nosa:nosa%40123@cluster0-av5ku.mongodb.net/test?retryWrites=true';
 // process.env.TODOAPP_MONGODB_URI
 mongoose.Promise = global.Promise;
 mongoose
-  .connect(dbhost || 'mongodb://localhost:27017/TodoApp', {
-    useNewUrlParser: true
-  })
+  .connect(
+    process.env.TODOAPP_MONGODB_URI || 'mongodb://localhost:27017/TodoApp',
+    {
+      useNewUrlParser: true
+    }
+  )
   .then(() => {
     console.log('connected to mongoDb');
   })
