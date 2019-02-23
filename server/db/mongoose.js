49021dd2b80bb874f1ab497ac11d047a1f010ba5
knowsarzehmeh <osarzeh@gmail.com>
2019-02-23 17:23:37 +0100

    Updated mongodb connection string

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,15 +1,14 @@
 const mongoose = require('mongoose');
 
-// const dbhost = 'mongodb+srv://nosa:nosa%40123@cluster0-av5ku.mongodb.net/test?retryWrites=true';
-
+const dbhost =
+  'mongodb://nosa:nosa%40123@cluster0-shard-00-00-av5ku.mongodb.net:27017,cluster0-shard-00-01-av5ku.mongodb.net:27017,cluster0-shard-00-02-av5ku.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
+// 'mongodb+srv://nosa:nosa%40123@cluster0-av5ku.mongodb.net/test?retryWrites=true';
+// process.env.TODOAPP_MONGODB_URI
 mongoose.Promise = global.Promise;
 mongoose
-  .connect(
-    process.env.TODOAPP_MONGODB_URI || 'mongodb://localhost:27017/TodoApp',
-    {
-      useNewUrlParser: true
-    }
-  )
+  .connect(dbhost || 'mongodb://localhost:27017/TodoApp', {
+    useNewUrlParser: true
+  })
   .then(() => {
     console.log('connected to mongoDb');
   })
