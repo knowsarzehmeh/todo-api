9d8e7466b7dad5c668ee813a2ed1af38ded7798b
knowsarzehmeh <osarzeh@gmail.com>
2019-02-24 08:58:04 +0100

    set environment variables for mongoDB

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,10 +1,5 @@
 const mongoose = require('mongoose');
-const userName = 'nosa';
-const password = encodeURIComponent('nosa@123');
-console.log(password);
-const dbhost = `mongodb://nosa:nosa%40123@cluster0-shard-00-00-av5ku.mongodb.net:27017,cluster0-shard-00-01-av5ku.mongodb.net:27017,cluster0-shard-00-02-av5ku.mongodb.net:27017/TodoApp?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true`;
-// 'mongodb+srv://nosa:nosa%40123@cluster0-av5ku.mongodb.net/test?retryWrites=true';
-// process.env.TODOAPP_MONGODB_URI
+
 const {
   TODOAPP_MONGODB_URI,
   REPLICA_SET,
