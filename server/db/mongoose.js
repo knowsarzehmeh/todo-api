f7758c7e8d8db7f6245b5e6de0dce6deed38a6ba
knowsarzehmeh <osarzeh@gmail.com>
2019-02-24 08:49:17 +0100

    defined env vatiables for mongo uri

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,14 +5,18 @@ console.log(password);
 const dbhost = `mongodb://nosa:nosa%40123@cluster0-shard-00-00-av5ku.mongodb.net:27017,cluster0-shard-00-01-av5ku.mongodb.net:27017,cluster0-shard-00-02-av5ku.mongodb.net:27017/TodoApp?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true`;
 // 'mongodb+srv://nosa:nosa%40123@cluster0-av5ku.mongodb.net/test?retryWrites=true';
 // process.env.TODOAPP_MONGODB_URI
+const {
+  TODOAPP_MONGODB_URI,
+  REPLICA_SET,
+  AUTH_SOURCE,
+  RETRY_WRITES
+} = process.env;
+const MONGODB_URI = `${TODOAPP_MONGODB_URI}&replicaSet=${REPLICA_SET}&authSource=${AUTH_SOURCE}&retryWrites=${RETRY_WRITES}`;
 mongoose.Promise = global.Promise;
 mongoose
-  .connect(
-    process.env.TODOAPP_MONGODB_URI || 'mongodb://localhost:27017/TodoApp',
-    {
-      useNewUrlParser: true
-    }
-  )
+  .connect(MONGODB_URI || 'mongodb://localhost:27017/TodoApp', {
+    useNewUrlParser: true
+  })
   .then(() => {
     console.log('connected to mongoDb');
   })
