abe33a046eee53e72c10b9fad3dc3d6a72d44a54
knowsarzehmeh <osarzeh@gmail.com>
2019-02-24 10:30:47 +0100

    updated mongodb url string

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,18 +1,21 @@
 const mongoose = require('mongoose');
 
-const MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-
-if (process.env.NODE_ENV === 'production') {
-  const {
-    TODOAPP_MONGODB_URI,
-    REPLICA_SET,
-    AUTH_SOURCE,
-    RETRY_WRITES
-  } = process.env;
-
-  MONGODB_URI = `${TODOAPP_MONGODB_URI}&replicaSet=${REPLICA_SET}&authSource=${AUTH_SOURCE}&retryWrites=${RETRY_WRITES}`;
+const {
+  TODOAPP_MONGODB_URI,
+  REPLICA_SET,
+  AUTH_SOURCE,
+  RETRY_WRITES
+} = process.env;
+if (
+  process.env.TODOAPP_MONGODB_URI &&
+  process.env.REPLICA_SET &&
+  process.env.AUTH_SOURCE &&
+  process.env.RETRY_WRITES
+) {
 }
-
+const MONGODB_URI =
+  `${TODOAPP_MONGODB_URI}&replicaSet=${REPLICA_SET}&authSource=${AUTH_SOURCE}&retryWrites=${RETRY_WRITES}` ||
+  'mongodb://localhost:27017/TodoApp';
 mongoose.Promise = global.Promise;
 mongoose
   .connect(MONGODB_URI, {
